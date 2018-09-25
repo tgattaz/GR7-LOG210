736df2a5e997ae253d994f937ca00bf7d5c63e17
julia peixoto <julia_cristina_cacau@hotmail.com>
2018-09-24 23:35:07 -0400

    1

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require("express");
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/app'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.logger());
+
+app.get('/',function (req,res) {
+    res.send('Hello wolrd!!');
+});
+
+var port = process.env.PORT || 8000;
+
+app.listen(port);
