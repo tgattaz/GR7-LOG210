c4ef28ca03bfd2e05f81364fd96f645044d20b8e
julia peixoto <julia_cristina_cacau@hotmail.com>
2018-09-26 13:06:27 -0400

    TEST

--- app.js
+++ app.js
@@ -3,23 +3,7 @@ var mysql = require('mysql');
 var app = express();
 
 
-var connection = mysql.createConnection({
-    host:'us-cdbr-iron-east-01.cleardb.net',
-    user:'b2af54710141cb',
-    password:'f361883b',
-    database:'heroku_56876da023ab330'
-});
-connection.connect();
-app.get('/', function(req, res) {
 
-    connection.query('SELECT * FROM equipe',function(err,rows,fields){
-        if(err){
-            console.log('error: ',err);
-            throw err;
-        }
-        Response.send(['Vous êtes à l\'accueil',rows]);
-    });
-});
 let port = process.env.PORT;
 if(port==null || port==""){
     port=8000;
