1585becc1dcbc0f3bbddd9711ec596389c4fa97d
Marvin Meite <marvin.meite.1@getsmtl.ca>
2018-10-08 11:29:59 -0400

    le front-end de cette exemple fonctionne mais le back-end doit etre debbug

---
+++ react-pos/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "./js/components/Header";
+import Main from "./js/components/Main";
+const App = () => (
+  <div>
+    <Main />
+  </div>
+);
+export default App;
