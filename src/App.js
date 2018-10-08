f349aca8794eecb85ae6cdd4040f709f8b6bb510
Prexilia <38744357+Prexilia@users.noreply.github.com>
2018-10-08 17:15:07 -0400

    test login

--- src/App.js
+++ src/App.js
@@ -19,6 +19,31 @@ class App extends Component {
           >
             Learn React
           </a>
+          <div className="container">
+              <div className="card"/>
+              <div className="card">
+                  <h1 className="title">Login</h1>
+                  <form onSubmit={this.handleSubmit}>
+                      <div className="input-container">
+                      <input name="uname" type="text" value={this.stateUser.uname} id="#uname" required="required"
+                                  onChange={this.handleChange}/>
+                          <label form="#unamelabel">Username</label>
+                          <div className="bar"/>
+                      </div>
+                      <div className="input-container">
+                      <input name="password" type="password" value={this.stateUser.password} id="#pass" required="required"
+                                  onChange={this.handleChange}/>
+                          <label form="#passlabel">Password</label>
+                            <div className="bar"/>
+                      </div>
+                      <div className="button-container">
+                          <button type="submit" value="Submit"><span>Go</span></button>
+                      </div>
+                      {<div className="footer"><a href="#">Forgot your password?</a></div> }
+                      
+                  </form>
+              </div>
+          </div>
         </header>
       </div>
     );
