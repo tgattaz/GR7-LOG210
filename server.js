532ee3dea315553a11ea56ce5f47668510684455
julia peixoto <julia_cristina_cacau@hotmail.com>
2018-10-11 01:39:59 -0400

    changement de 'build' pour 'public'

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ if(!dev){
     app.use(compression());
     app.use(morgan('common'));
 
-    app.use(express.static(path.resolve(__dirname,'build')));
+    app.use(express.static(path.resolve(__dirname,'public')));
 
     app.get('*',(req,res) => {
-        res.sendFile(path.resolve(__dirname,'build','index.html'));
+        res.sendFile(path.resolve(__dirname,'public','index.html'));
     });
 }
 
