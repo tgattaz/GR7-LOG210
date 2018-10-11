951f3c75f2b9333a6fba2bdc1ae4217deac6e115
marvinmeite <43545737+marvinmeite@users.noreply.github.com>
2018-10-10 20:23:11 -0400

    Update App.js
    dessoler voici la bonne version

--- src/App.js
+++ src/App.js
@@ -1,54 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import { AppRegistry, Text, StyleSheet } from 'react-native';
+
 import './App.css';
+import { auth } from "./services/login"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          
-        </header>
-        <div className="container">
-              <div className="card"/>
-              <div className="card">
-                  <h1 className="title">Login</h1>
-                  <form onSubmit={this.handleSubmit}>
-                      <div className="input-container">
-                      <input name="uname" type="text" value={this.stateUser.uname} id="#uname" required="required"
-                                  onChange={this.handleChange}/>
-                          <label form="#unamelabel">Username</label>
-                          <div className="bar"/>
-                      </div>
-                      <div className="input-container">
-                      <input name="password" type="password" value={this.stateUser.password} id="#pass" required="required"
-                                  onChange={this.handleChange}/>
-                          <label form="#passlabel">Password</label>
-                            <div className="bar"/>
-                      </div>
-                      <div className="button-container">
-                          <button type="submit" value="Submit"><span>Go</span></button>
-                      </div>
-                      {<div className="footer"><a href="#">Forgot your password?</a></div> }
-                      
-                  </form>
-              </div>
-          </div>
-      </div>
-    );
+export default class Login extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {uname: '', password: ''};
+  
   }
-}
+  //higher order components (HOC)  factoriser code :erreur , page loading 
+  //faire les test avec jest render %
+  //alller par featcher 
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
 
-export default App;
+    this.setState({
+      [name]: value
+    });
+
+  }
+  
+  handleSubmit = (event) => {
+    //   alert('A username and password  was submitted: ' + this.state.uname + this.state.password);
+      event.preventDefault();
+      auth(this.state);
+  }
+  
+  render() {
+  
+      return (
+          <div>
+              <header titleName={"Login"}>
+                  <div className="container">
+                      <div className="card"/>
+                      <div className="card">
+                          <h1 className="title">Login</h1>
+                          <form onSubmit={this.handleSubmit}>
+                              <div className="input-container">
+                                  <input name="uname" type="text" value={this.state.uname} id="#uname" required="required"
+                                          onChange={this.handleChange}/>
+                                  <label form="#unamelabel">Username</label>
+                                  <div className="bar"/>
+                              </div>
+                              <div className="input-container">
+                                  <input name="password" type="password" value={this.state.password} id="#pass" required="required"
+                                         onChange={this.handleChange}/>
+                                  <label form="#passlabel">Password</label>
+                                  <div className="bar"/>
+                              </div>
+                              <div className="button-container">
+                                  <button type="submit" value="Submit"><span>Go</span></button>
+                              </div>
+                              <div className="footer"><a href="#">Forgot your password?</a></div>
+                          </form>
+                      </div>
+                  </div>
+              </header>
+              <footer/>
+          </div>
+      );
+  }
+  }
