6db15f16feeebbd2616eb44afd5f2a5296e778bf
julia peixoto <julia_cristina_cacau@hotmail.com>
2018-10-18 15:35:42 -0400

    initial comit
    importaion du projet login test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//react router tuto ici 
+// react router tuto ici 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
