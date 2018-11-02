15fcd87fed50c09cacb522fa403979c6574088da
marvinmeite <43545737+marvinmeite@users.noreply.github.com>
2018-11-02 14:51:40 -0400

    essaye de test non fonctionnels

--- src/test/RechercheReferent.test.js
+++ src/test/RechercheReferent.test.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import RechercheReferent from '../services/RechercheReferent';
-import TestUtils from 'react-addons-test-utils';
+//import ReactDOM from 'react-dom';
+import RechercheReferentForm from '../components/forms/selectionForms/RechercheReferentForm';
+import Foo from './Foo';
 
+import {shallow} from 'enzyme'
+//import TestUtils from 'react-addons-test-utils';
+
+describe('Recherche Referent Renders without crashing',() => {
 it('Recherche Referent Renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<RechercheReferent />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const wrapper = shallow( < RechercheReferentForm / > );
+ // const rechercherState = wrapper.state().rechercher
+  expect(wrapper.find(Foo)).to.have.lengthOf(3);
+  //expect(rechercherState).toEqual(0)
+  
+})
+})
+
+/*const div = document.createElement('div');
+  ReactDOM.render( < RechercheReferentPage / > , div);
+  ReactDOM.unmountComponentAtNode(div); ;
 
 it('Recherche rerent trouve', () => {
   const div = document.createElement('div');
-  const a = <RechercheReferent />;  
+  const a = < RechercheReferentPage / > ;
   
   ReactDOM.render(a, div, () => {
   });
   ReactDOM.unmountComponentAtNode(div);
-});
+}); */
