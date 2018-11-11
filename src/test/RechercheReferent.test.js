3937551758b6792f9fea5fd225d99e90ded4a7f3
Eymen Rayane Djadel <eymen-rayane.djadel.1@ens.etsmtl.ca>
2018-11-11 14:51:25 -0500

    Nouveaux tests back

--- src/test/RechercheReferent.test.js
+++ src/test/RechercheReferent.test.js
@@ -3,21 +3,16 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 //import ReactDOM from 'react-dom';
 import RechercheReferentForm from '../components/forms/selectionForms/RechercheReferentForm';
-//import Foo from './Foo';
-
 import { shallow, render, mount, configure } from 'enzyme';
 configure({ adapter: new Adapter() });
-//import TestUtils from 'react-addons-test-utils';
+
 
 describe('Recherche Referent Renders without crashing',() => {
 it('Recherche Referent Renders without crashing', () => {
   const wrapper = shallow( < RechercheReferentForm /> );
- // const rechercherState = wrapper.state().rechercher
   expect(wrapper).toMatchSnapshot();
-  //expect(rechercherState).toEqual(0)
-  
-})
-})
+});
+});
 
 /*const div = document.createElement('div');
   ReactDOM.render( < RechercheReferentPage / > , div);
