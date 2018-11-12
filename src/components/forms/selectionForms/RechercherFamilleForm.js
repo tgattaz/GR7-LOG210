62837e71713df6dd981de01e9754fbca5d70ad62
marvinmeite <43545737+marvinmeite@users.noreply.github.com>
2018-11-12 09:54:39 -0500

    fontEnd note rechercherFamille

--- src/components/forms/selectionForms/RechercherFamilleForm.js
+++ src/components/forms/selectionForms/RechercherFamilleForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { Form, Button, Table } from "semantic-ui-react";
+import AddNoteForm from "../../forms/addOrUpdateForms/AddNoteForm";
 
 export default class RechercheFamilleForm extends React.Component {
   state = {
@@ -41,41 +42,52 @@ export default class RechercheFamilleForm extends React.Component {
       });
   };
 
+  selectChannel = e => {
+    this.setState({
+      choixFamille: e.target.innerHTML,
+      toAddNote: true
+    });
+  };
+
   render() {
-    return (
-      <div className="rechercheFamille">
-        <Form onSubmit={this.handleSumit}>
-          <h2> Rercherche d une famille</h2>
-          <Form.Field>
-            <label>
-              Recherche par numéro de dossier, parent, tuteur, référent, enfant,
-              une viste supervisée, un échange de garde : <br />
-              <input
-                type="text"
-                name="recherche"
-                onChange={this.handleChangeRecherche}
-              />
-            </label>
-          </Form.Field>
-          <Button primary> Rechercher </Button>
-        </Form>
-        <Table celled padded>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell> Numéro de famille </Table.HeaderCell>
-              <Table.HeaderCell> Nom de la famille </Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {this.state.familles_trouves.map(famille => (
-              <Table.Row key={famille.noFamille}>
-                <Table.Cell> {famille.noFamille} </Table.Cell>
-                <Table.Cell> {famille.nomFamille} </Table.Cell>
+    if (this.state.toAddRef === true) {
+      return <AddNoteForm choixFamille={this.state.choixFamille} />;
+    } else {
+      return (
+        <div className="rechercheFamille">
+          <Form onSubmit={this.handleSumit}>
+            <h2> Rercherche d une famille</h2>
+            <Form.Field>
+              <label>
+                Recherche par numéro de dossier, parent, tuteur, référent,
+                enfant, une viste supervisée, un échange de garde : <br />
+                <input
+                  type="text"
+                  name="recherche"
+                  onChange={this.handleChangeRecherche}
+                />
+              </label>
+            </Form.Field>
+            <Button primary> Rechercher </Button>
+          </Form>
+          <Table celled padded>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell> Numéro de famille </Table.HeaderCell>
+                <Table.HeaderCell> Nom de la famille </Table.HeaderCell>
               </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </div>
-    );
+            </Table.Header>
+            <Table.Body>
+              {this.state.familles_trouves.map(famille => (
+                <Table.Row key={famille.noFamille}>
+                  <Table.Cell> {famille.noFamille} </Table.Cell>
+                  <Table.Cell> {famille.nomFamille} </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
+      );
+    }
   }
 }
