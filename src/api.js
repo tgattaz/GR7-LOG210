d405d712ac3b1ab71755a7f39a5bff6bf2c91607
tesour <40697060+tesour@users.noreply.github.com>
2018-12-03 12:27:41 -0500

    Revert "Merge branch 'master' into tests_front_marvin_2"
    This reverts commit 2cca724c5125f1f3d598091d91aa6736c5b1fa16, reversing
    changes made to defece87cb0c8ff286b27fe5910b6c6b800cc83f.

--- src/api.js
+++ src/api.js
@@ -4,13 +4,5 @@ export default {
     user: {
         login: (credentials) => 
         axios.post("/api/auth",{ credentials }).then(res => res.data.user)
-    },
-    organisme:{
-        getOrga: (info)=> 
-        axios.post("/api/get",{ info }).then(res => res.data.organisme)
-    },
-    pointDeService:{
-        insert: (info)=>
-        axios.post("/api/insertPointDeService",{ info }).then(res =>res.data.pointDeService)
     }
 }
